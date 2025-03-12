73388307d36d2a232b88e5a8254b305684957935
Yunsik Ohm <yunsikohm@gmail.com>
2025-03-11 19:26:25 -0700

    heor.tsx - set up the framework and files hierarchy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+// React entry point
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
