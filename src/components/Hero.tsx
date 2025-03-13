1223e47d9179a2425b4f3f1c9dae83865b851274
Yunsik Ohm <yunsikohm@gmail.com>
2025-03-13 09:53:51 -0700

    hero.tsx - set up the placeholders for texts for different screen sizes. also todo lists were set for the next work.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ import InstagramIcon from "./icons/InstagramIcon";
 const Hero = () => {
   return (
     <div className="w-full h-full flex flex-col justify-center bg-white-bg-1 dark:bg-dark-bg-3 p-4 border-4">
-      <h2 className="mb-6 font-mono text-lg text-neutral-800 dark:text-neutral-300">
+      <h2 className="mb-6 font-mono text-base text-left text-neutral-800 dark:text-neutral-300">
         Hi, my name is
       </h2>
       <h1 className="mb-4 font-sans text-5xl font-bold text-blue-600 dark:text-blue-500 ">
@@ -19,12 +19,24 @@ const Hero = () => {
         <div>Engineer</div>
         <div>Writer</div>
       </h2>
-      <p className="max-[300px]:hidden mb-6 text-base text-neutral-800 dark:text-neutral-300 w-8/9 sm:w-7/9 md:w-6/9 lg:w-5/9">
-        <div className="small:">
+      <p className="mb-6 text-base text-neutral-800 dark:text-neutral-300 w-8/9 sm:w-7/9 md:w-6/9 lg:w-5/9">
+        <div className="hidden max-sm:block">
           I’m passionate about applying machine learning to real-world problems.
           Currently, I’m developing a framework to optimize material composition
           for wearable technologies.
         </div>
+        <div className="hidden sm:max-lg:block">
+          Placeholder for tablet text display.
+        </div>
+        <div className="hidden lg:max-2k:block">
+          Placeholder for common desktop text display.
+        </div>
+        <div className="hidden 2k:block">
+          Placeholder for larger desktop text display.
+        </div>
+        {/* TODO: Need to work on the texts for different display size. */}
+        {/* TODO: Need to work on layout of the texts. */}
+        {/* TODO: Need to work on social icons. */}
       </p>
       <button className="social-container bg-blue-500">Save Changes</button>
       <a
