a4be9c8fcafa0b9e7552ef41f71339a127073b5b
Yunsik Ohm <yunsikohm@gmail.com>
2025-03-13 22:01:47 -0700

    hero.tsx - responsive behavior set up.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div>
       <Hero />
+      <div className="hidden max-[200px]:block w-full h-full min-h-screen bg-neutral-100 dark:bg-neutral-850 text-white-bg-1 dark:bg-dark px-10 py-10">
+        This screen is too small to show any items.
+      </div>
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,19 +7,19 @@ import InstagramIcon from "./icons/InstagramIcon";
 
 const Hero = () => {
   return (
-    <div className="w-full h-full flex flex-col justify-center bg-white-bg-1 dark:bg-dark-bg-3 p-4 border-4">
-      <h2 className="mb-6 font-mono text-base text-left text-neutral-800 dark:text-neutral-300">
+    <div className="max-[200px]:hidden w-full h-full min-h-screen flex flex-col justify-center bg-white-bg-1 dark:bg-dark-bg-3 max-sm:px-10 sm:max-lg:px-20 lg:max-2k:px-30 2k:px-40">
+      <h2 className="hero-hi mb-6 font-mono text-base text-left text-neutral-800 dark:text-neutral-300">
         Hi, my name is
       </h2>
-      <h1 className="mb-4 font-sans text-5xl font-bold text-blue-600 dark:text-blue-500 ">
+      <h1 className="hero-name mb-4 font-sans text-5xl font-bold text-blue-600 dark:text-blue-500 ">
         Yunsik Ohm
       </h1>
-      <h2 className="mb-6 font-mono text-4xl font-bold text-neutral-900 dark:text-white-bg-1">
+      <h2 className="hero-title mb-6 font-mono text-4xl font-bold text-neutral-900 dark:text-white-bg-1">
         <div>Programmer</div>
         <div>Engineer</div>
         <div>Writer</div>
       </h2>
-      <p className="mb-6 text-base text-neutral-800 dark:text-neutral-300 w-8/9 sm:w-7/9 md:w-6/9 lg:w-5/9">
+      <p className="hero-text mb-6 text-base text-neutral-800 dark:text-neutral-300 w-8/9 sm:w-7/9 md:w-6/9 lg:w-5/9">
         <div className="hidden max-sm:block">
           I’m passionate about applying machine learning to real-world problems.
           Currently, I’m developing a framework to optimize material composition
@@ -38,39 +38,40 @@ const Hero = () => {
         {/* TODO: Need to work on layout of the texts. */}
         {/* TODO: Need to work on social icons. */}
       </p>
-      <button className="social-container bg-blue-500">Save Changes</button>
-      <a
-        href="https://github.com/yo-resistor"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="social-container"
-      >
-        <GitHubIcon />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/yohm/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="social-container"
-      >
-        <LinkedInIcon />
-      </a>
-      <a
-        href="https://scholar.google.com/citations?user=MdnAj8IAAAAJ&hl=en&oi=ao"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="social-container"
-      >
-        <GoogleScholarIcon />
-      </a>
-      <a
-        href="https://www.instagram.com/yunsik_ohm/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="social-container"
-      >
-        <InstagramIcon />
-      </a>
+      <div className="flex items-center gap-x-4 sm:hidden">
+        <a
+          href="https://github.com/yo-resistor"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-container w-[24px]"
+        >
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/yohm/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-container w-[24px]"
+        >
+          <LinkedInIcon />
+        </a>
+        <a
+          href="https://scholar.google.com/citations?user=MdnAj8IAAAAJ&hl=en&oi=ao"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-container w-[24px]"
+        >
+          <GoogleScholarIcon />
+        </a>
+        <a
+          href="https://www.instagram.com/yunsik_ohm/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-container w-[24px]"
+        >
+          <InstagramIcon />
+        </a>
+      </div>
     </div>
     //   <section className="w-full  flex flex-col justify-center min-h-screen px-10 md:px-40 sm:px-20 bg-white dark:bg-dark-bg-3">
     //     <motion.h2
