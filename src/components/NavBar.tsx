3639d5e2efabf3ceeabfc210df4dfade3c3269c1
Yunsik Ohm <yunsikohm@gmail.com>
2025-03-19 14:00:00 -0700

    navbar.tsx - updated with todo lists.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -52,41 +52,10 @@ export default function NavBar() {
   );
 }
 
-// export default function NavBar() {
-//   const getPreferredTheme = (): "light" | "dark" => {
-//     const storedTheme = localStorage.getItem("theme");
-//     if (storedTheme === "light" || storedTheme === "dark") {
-//       return storedTheme;
-//     }
-//     return window.matchMedia("(prefers-color-scheme: dark)").matches
-//       ? "dark"
-//       : "light";
-//   };
-
-//   const [theme, setTheme] = useState<"light" | "dark">(getPreferredTheme);
-
-//   useEffect(() => {
-//     document.documentElement.setAttribute("data-theme", theme); // Ensures Tailwind detects theme change
-//     document.documentElement.classList.remove("light", "dark");
-//     document.documentElement.classList.add(theme);
-//     localStorage.setItem("theme", theme);
-//   }, [theme]);
-
-//   const toggleTheme = () => {
-//     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
-//   };
-
-//   return (
-//     <nav className="flex justify-between items-center p-4 bg-white dark:bg-black text-black dark:text-white border-b border-gray-300 dark:border-gray-700">
-//       <span className="text-lg font-bold">NavBar</span>
-//       <button
-//         onClick={toggleTheme}
-//         className="peer px-4 py-2 rounded-lg transition-colors duration-300 bg-gray-800 text-white dark:bg-gray-300 dark:text-black hover:bg-gray-700 dark:hover:bg-gray-400"
-//       >
-//         Toggle Theme
-//       </button>
-//     </nav>
-//   );
-// }
-
-// TODO: navigation bar with darkmode toggle button.
+// TODO: Apply ThemeToggle to the NavBar.
+// TODO: Apply logo and other pages to the NavBar.
+// TODO: Apply shade for the NavBar as well -> Take a look at yunsikohm.com
+// TODO: Mobile friendly UI/UX.
+// TODO: Hamburger that contains other pages. ThemeToggle won't go inside. Maybe we can think about simplifying ThemeToggle as just one icon without the slider bar.
+// TODO: When Hamburger is activated, blur the background.
+// TODO: When users click/touch outside of the popped hamburger section or navigation bar, deactivate the popped hamburger.
