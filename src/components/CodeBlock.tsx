0b4f34adf4410ff37f2f573137b9246995899350
Yunsik Ohm <yunsikohm@gmail.com>
2025-04-21 09:52:42 -0700

    tried to make some changes for blog posts. this needs to be fixed to show the contetns in the code snippets with proper themes that I picked in TODO. Blog page needs to be implemented properly as well.

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,17 +1,47 @@
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+// interface CodeBlockProps {
+//   children: string;
+//   language?: string;
+// }
+
+// const CodeBlock = ({ children, language = "bash" }: CodeBlockProps) => {
+//   return (
+//     <SyntaxHighlighter language={language} style={dracula}>
+//       {children.trim()}
+//     </SyntaxHighlighter>
+//   );
+// };
+
+// export default CodeBlock;
+
+import React, { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/components/prism-python"; // import the language you need
+import "prismjs/themes/prism-tomorrow.css"; // import the theme
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-bash";
 
 interface CodeBlockProps {
-  children: string;
-  language?: string;
+  code: string;
+  language: string;
 }
 
-const CodeBlock = ({ children, language = "bash" }: CodeBlockProps) => {
+const CodeBlock: React.FC<CodeBlockProps> = ({ code, language }) => {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
   return (
-    <SyntaxHighlighter language={language} style={dracula}>
-      {children.trim()}
-    </SyntaxHighlighter>
+    <pre
+      className={`language-${language} rounded-lg p-4 overflow-auto text-sm`}
+    >
+      <code className={`language-${language}`}>{code}</code>
+    </pre>
   );
 };
 
 export default CodeBlock;
+
+//TODO: Select one-dark and one-light in https://react-syntax-highlighter.github.io/react-syntax-highlighter/demo/prism.html for theme.
